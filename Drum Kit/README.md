ea5d6287965a061c8528e649bf28b12f6090ba48
Eyesore123 <joni.putkinen@protonmail.com>
2023-11-15 17:28:31 +0200

    only good changes

---
+++ Drum Kit/README.md
@@ -0,0 +1,8 @@
+# Joni-s-portfolio
+Joni's portfolio
+
+I'm sorry, but my portfolio is still pretty much at the first stage. I'm working on multiple projects at the moment. I'll add those once they are ready.
+
+br, 
+
+Joni
---
+++ Drum Kit/list.js
@@ -0,0 +1,55 @@
+// Create a "close" button and append it to each list item
+
+
+var listVariable = document.getElementsByClassName("list-item");
+var i; 
+for (i = 0; i < listVariable.length; i++) {
+    var span = document.createElement("span");
+    var cross = document.createTextNode("\u274C");
+    span.className = "close";
+    span.appendChild(cross);
+    listVariable[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Add a "checked" symbol when clicking on a list item
+
+var addCheck = document.getElementsByClassName("list-item");
+var i;
+for (i = 0; i < addCheck.length; i++) {
+    addCheck[i].onclick = function() {
+        this.classList.toggle("checked");
+    }
+}
+
+
+// Create a new list item when clicking on the "Add" button
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("someInput").value;
+    var textNode = document.createTextNode(inputValue);
+    li.appendChild(textNode);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("to-do-list").appendChild(li);
+    }
+    document.getElementById("someInput").value = "";
+    
+    var span = document.createElement("span");
+    var cross = document.createTextNode("\u274C");
+    span.className = "close";
+    span.appendChild(cross);
+    li.appendChild(span);
+}
