07da503d34ee94f7807c6e0eb0d8b3a5ad55fcc8
Eyesore123 <joni.putkinen@protonmail.com>
2023-12-03 16:03:38 +0200

    -

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import axios from "axios";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const app = express();
-const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.static("c:/Users/Joni/Desktop/Joni's Website/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}`));
 
-// Set the view engine to EJS
+// Set the views directory
+
+
+app.set('views', `${__dirname}/views`);
+// Set the view engine
 app.set('view engine', 'ejs');
 
-// Define routes
+// Define the GET route for rendering the testPage.ejs
 app.get('/', (req, res) => {
-  res.render('test page.ejs');
+  res.render('test page.ejs', { randomMeme: ""});
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+
+app.post("/gimme", async (req, res) => {
+    try {
+        const response = await axios.get("https://meme-api.com/gimme");
+        const randomMeme = response.data.url;
+        res.render('test page.ejs', { randomMeme });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error fetching random meme");
+    }
 });
+
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
