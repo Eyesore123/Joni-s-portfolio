a3baf74182726f5c81a8bd4f390c8e8dff7caca8
Eyesore123 <146933644+Eyesore123@users.noreply.github.com>
2024-12-11 13:59:01 +0200

    Update Approuter.js

--- src/Approuter.js
+++ src/Approuter.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage'; // Import your LandingPage component
 import QuizApp from './components/App'; // Import your QuizApp component
 import './components/styles.css';
 
 const Approuter = () => {
     return (
-        <HashRouter basename="/Joni-s-portfolio/quiz-react-app">  {/* Set the basename for subdirectory */}
+        <BrowserRouter>
             <Routes>  {/* Routes wrapper for the individual routes */}
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/quiz" element={<QuizApp />} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 };
 
